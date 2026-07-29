6473dc179a9d132346546d811c489ff5f236dd9e
Ryleeee1337 <2025080221@psu.palawan.edu.ph>
2026-07-29 23:04:51 +0800

    Create Problem2.cs

---
+++ Problem2.cs
@@ -0,0 +1,10 @@
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 0)
+{
+    Console.WriteLine($"The number {number} is positive.");
+}
+else
+{
+    Console.WriteLine($"The number {number} is negative.");
+}
