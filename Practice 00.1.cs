01ce945f135b0ed1c137c5cfe04563135ecadb26
Ryleeee1337 <2025080221@psu.palawan.edu.ph>
2026-07-29 23:45:39 +0800

    Create Practice 00.1.cs

---
+++ Practice 00.1.cs
@@ -0,0 +1,8 @@
+Console.Write("Enter first whole number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second whole number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = num1 + num2;
+Console.WriteLine($"Sum: {sum}");
