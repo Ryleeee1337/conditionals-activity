b3041998e268f4712d970938e3907dc73701d28d
Ryleeee1337 <2025080221@psu.palawan.edu.ph>
2026-07-29 23:46:25 +0800

    Create Practice 00.2.cs

---
+++ Practice 00.2.cs
@@ -0,0 +1,11 @@
+Console.Write("Enter Principal amount: ");
+double principal = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Rate of interest (%): ");
+double rate = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Time (in years): ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+double interest = (principal * rate * time) / 100;
+Console.WriteLine($"Simple Interest: {interest}");
